d72e21f89c59e71ea81a528fe1096db3bbb71a26
Gokul Anand S <sgokulanand0505@gmail.com>
2024-12-31 20:12:36 +0530

    core java program

---
+++ String_concat.java
@@ -0,0 +1,10 @@
+class String_concat{
+    public static void main(String[] args) {
+        String str="Hello";
+        int a=5;
+        System.out.println(str+a);//String concatination
+        char b='b';
+        System.out.println(str+b);
+        System.out.println(a+b);// addition of int and ascii value of character of b
+    }
+}
